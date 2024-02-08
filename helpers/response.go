7000d084c2ded31224a565045649bebd0f2cc58f
PailosNicolas <Pailosnicolas@gmail.com>
2024-02-08 23:04:40 +0000

    Added response helpers

---
+++ helpers/response.go
@@ -0,0 +1,29 @@
+package helpers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type returnError struct {
+	Error string `json:"error"`
+}
+
+func RespondWithError(w http.ResponseWriter, code int, msg string) {
+	response := returnError{
+		Error: msg,
+	}
+	RespondWithJSON(w, code, response)
+}
+
+func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.WriteHeader(code)
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Write(dat)
+}
